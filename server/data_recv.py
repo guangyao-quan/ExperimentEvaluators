20be822c88f12a673fcda04ef42116344d394db1
guangyao-quan <2522089516quan@gmail.com>
2023-05-19 17:20:55 +0200

    Implement base structure of data subm and recv

---
+++ server/data_recv.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# /submit-form URL, route listens for POST requests to the URL
+@app.route('/submit-form', methods=['POST'])
+def submit_form():
+    # form_data is a dictionary, extract data from the request
+    form_data = request.form
+    # TODO: process form_data
+    # ... calling a model to evaluate the expriment results
+    # ... saving the results to 'results'
+    response_data = {'results': 'results'}
+    # return the response data as JSON
+    return jsonify(response_data)
