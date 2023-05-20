d35913e2cdf3fc563f48e0ef440a2d0edcb7b67a
guangyao-quan <2522089516quan@gmail.com>
2023-05-20 21:37:21 +0200

    Install some dependencies

--- server/data_recv.py
+++ server/data_recv.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def submit_form():
     # form_data is a dictionary, extract data from the request
     form_data = request.form
+
     # TODO: process form_data
     # ... calling a model to evaluate the experiment results
     # ... saving the results to 'results'
     response_data = {'results': 'results'}
+
     # return the response data as JSON
     return jsonify(response_data)
