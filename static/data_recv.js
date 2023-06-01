dc8d9e98116dcc56f9220e9ba586615dcac71f33
Guangyao Quan <2522089516quan@gmail.com>
2023-06-01 16:40:16 +0200

    Integrate the feedback form with the data_recv.js

--- static/data_recv.js
+++ static/data_recv.js
@@ -1,13 +1,23 @@
+function populateFeedbackForm(data) {
+    var feedbackForm = document.getElementById("experimentFeedback");
+  
+    feedbackForm.question.value = data.question;
+    feedbackForm.hypothesis.value = data.hypothesis;
+    feedbackForm.material.value = data.material;
+    feedbackForm.implementation.value = data.implementation;
+    feedbackForm.observation.value = data.observation;
+    feedbackForm.result.value = data.result;
+}
+  
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'url_to_server_endpoint', true);
 xhr.setRequestHeader('Content-Type', 'application/json');
-
+  
 xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE) {
         if (xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
-            // process the JSON data received from the server
-            console.log(response);
+            populateFeedbackForm(response);
         } else {
             console.error('Request failed. Status:', xhr.status);
         }
@@ -15,8 +25,7 @@ xhr.onreadystatechange = function() {
 };
 
 xhr.send();
-
-
+  
 /*
 
 from flask import Flask, request, jsonify
